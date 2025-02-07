e4af48e523493d6df608c1ece08eb9f4a922798a
Lucas0105 <dnjswo105@gmail.com>
2025-02-07 21:15:20 +0900

    [TypeScript] 타입 설정 해주는 방법

--- TypeScript/fetchJson/index.ts
+++ TypeScript/fetchJson/index.ts
@@ -1,6 +1,22 @@
 import axios from 'axios';
 
+interface Todo {
+    id:number;
+    title:string;
+    completed:boolean;
+}
+
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 axios.get(url).then(response => {
-    console.log(response.data);
-})
+    const tood = response.data as Todo;
+
+    const id = tood.id;
+    const title = tood.title;
+    const completed = tood.completed;
+
+    console.log(`
+       The Todo with ID: ${id}
+       Has a title of: ${title}
+       Is it finished? ${completed}
+        `)
+});

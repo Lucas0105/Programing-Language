5be07f86b3ad12934139b3b7af548dd34091a4fb
Lucas0105 <dnjswo105@gmail.com>
2025-02-08 20:52:35 +0900

    [TypeScript] 구조분해

--- TypeScript/feature/function.ts
+++ TypeScript/feature/function.ts
@@ -22,3 +22,15 @@ const logger = (message:string): void => {
 const throwError = (message: string) : never => {
     throw new Error(message);
 }
+
+const todayWeather = {
+    date: new Date(),
+    weather: 'sunny'
+}
+
+const logWeather = ({date, weather}:{date:Date; weather:string;}):void => {
+console.log(date);
+console.log(weather);
+}
+
+logWeather(todayWeather);
